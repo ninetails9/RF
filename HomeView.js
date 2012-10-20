b28ea013a520fd686df5cdf57c495682a1787368
ninetails9 <ninetails@ninetails.kr>
2012-10-20 17:24:58 +0900

    init

---
+++ HomeView.js
@@ -0,0 +1,9 @@
+Ext.define("HomeView", {
+	extend:"Ext.Panel",
+	xtype: 'homeView',
+	config: {
+		title: "Home",
+		style: "background-color: red; color:white;",
+		html: "homeView"
+	}
+});
---
+++ ListView.js
@@ -0,0 +1,9 @@
+Ext.define("ListView", {
+	extend:"Ext.Panel",
+	xtype: 'listView',
+	config: {
+		title: "List",
+		style: "background-color: blue; color:white;",
+		html: "listView"
+	}
+});
